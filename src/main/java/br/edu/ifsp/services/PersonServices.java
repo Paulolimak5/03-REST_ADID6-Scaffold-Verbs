13e7e7bf55273f04e04e057aaf1d409780c138cc
Paulo <p.manoel.m@gmail.com>
2021-04-26 23:00:38 -0300

    id e lista

---
+++ src/main/java/br/edu/ifsp/services/PersonServices.java
@@ -0,0 +1,46 @@
+package br.edu.ifsp.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.edu.ifsp.model.Person;
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Antonio");
+		person.setLastName("Angelo");
+		person.setAddress("Avenida Salgado Filho, 3501 - Guarulhos");
+		person.setGender("Male");
+		return person;
+		
+	}
+	
+	public List<Person> findAll(){
+		List<Person> persons = new ArrayList<Person>();
+		for(int i=0; i<10; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Nome " + i);
+		person.setLastName("Sobrenome " + i);
+		person.setAddress("Endereço " + i);
+		person.setGender("Male");
+		return person;
+	}
+
+}
