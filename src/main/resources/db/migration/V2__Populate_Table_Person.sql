8f0f49ef726a67f6501cde4c66085fb4f027c97b
Paulo <p.manoel.m@gmail.com>
2021-06-14 23:04:06 -0300

    Migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(1,'Rua sargento Manuel Chagas','Paulo Manoel','Male','Gonçalves de Lima'),
+(3,'Rua do Mato','Ana','Female','Paula');
